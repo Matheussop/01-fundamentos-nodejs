dfd3c9bd6a313d774110dbc1d1b5156bc0f22849
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-09-07 21:57:05 -0300

    feat: Database with json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -9,6 +9,8 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
     return res
       .end(JSON.stringify(users))
   }
@@ -16,14 +18,14 @@ const server = http.createServer(async (req, res) => {
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user ={
       id: 1,
       name,
       email,
-    })
+    }
 
-    return res.writeHead(201).end()
   }
+  database.insert('users', user)
 
   return res.writeHead(404).end()
 })
